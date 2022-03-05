c1689866d6159d0782902e4bba3ad0c8bbf01cce
rodriguez-michael <github@michaelrodriguez.dev>
2022-03-04 18:19:35 -0600

    connect express and run on localhost

--- server/index.js
+++ server/index.js
@@ -3,14 +3,19 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
+
+//Routes
+app.use('/posts', postRoutes)
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 // https://www.mongodb.com/cloud/atlas
-const CONNECTION_URL = 'database connection string';
+const CONNECTION_URL = '';
 const PORT = process.env.PORT || 4000;
 
 mongoose.connect(CONNECTION_URL)
