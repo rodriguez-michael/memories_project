2671083f507ed5dd8798d7a3c031449980d9b398
rodriguez-michael <github@michaelrodriguez.dev>
2022-03-04 19:04:48 -0600

    added server side logic to make a real post

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { getPosts } from '../controllers/posts.js';
+import { getPosts, createPost } from '../controllers/posts.js';
 
 const router = express.Router();
 
 //http://localhost:4000/posts
 router.get('/', getPosts);
+router.get('/', createPost);
 
 export default router;
