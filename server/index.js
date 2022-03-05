927d97b1f5807a3dccc2570022edbfe2bbf4ffb2
rodriguez-michael <github@michaelrodriguez.dev>
2022-03-04 18:06:58 -0600

    connect to mongoDB

--- server/index.js
+++ server/index.js
@@ -2,3 +2,17 @@ import express from "express";
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// https://www.mongodb.com/cloud/atlas
+const CONNECTION_URL = 'database connection string';
+const PORT = process.env.PORT || 4000;
+
+mongoose.connect(CONNECTION_URL)
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
